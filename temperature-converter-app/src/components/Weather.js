c69b2a5b3fcfd6bf22dddbb070d30def93c27a7f
MA RONG <marongreceive@gmail.com>
2024-06-28 17:43:18 +0900

    add picturefetch function

--- temperature-converter-app/src/components/Weather.js
+++ temperature-converter-app/src/components/Weather.js
@@ -4,11 +4,13 @@ import { useSelector, useDispatch } from "react-redux";
 import {
   fetchUserCity,
   fetchWeatherData,
+  fetchCityPhoto
 } from "../store/modules/weatherData";
 
 function Weather() {
   const cityName = useSelector((state) => state.weatherdata.cityName);
   const weatherData = useSelector((state) => state.weatherdata.weatherData);
+  const cityPhotoUrl = useSelector((state) => state.weatherdata.cityPhoto);
   const dispatch = useDispatch();
   const [initialLoad, setInitialLoad] = useState(true);
 
@@ -16,8 +18,10 @@ function Weather() {
     if (initialLoad) {
       dispatch(fetchUserCity()).then(() => setInitialLoad(false));
       dispatch(fetchWeatherData()); 
+      dispatch(fetchCityPhoto());
     } else if (cityName) {
       dispatch(fetchWeatherData(cityName));
+      dispatch(fetchCityPhoto());
     }
   }, [dispatch, cityName, initialLoad]);
 
@@ -47,7 +51,12 @@ function Weather() {
       <h2 className="description">
         {weatherData ? weatherData.weather[0].description : "Loading..."}
       </h2>
-      <div className="weatherpic">{cityName}</div>
+      <div
+        className="weatherpic"
+        style={{ backgroundImage: `url(${cityPhotoUrl})` }}
+      >
+        <span>{cityName}</span>
+      </div>
     </div>
   );
 }
--- temperature-converter-app/src/store/modules/weatherdata.js
+++ temperature-converter-app/src/store/modules/weatherdata.js
@@ -7,6 +7,7 @@ const weatherDataStore = createSlice({
   initialState: {
     cityName: "",
     weatherData: "",
+    cityPhoto: "",
   },
   reducers: {
     // 在recucer里面设置action
@@ -16,10 +17,13 @@ const weatherDataStore = createSlice({
     setWeatherData: (state, action) => {
       state.weatherData = action.payload;
     },
+    setCityPhoto: (state, action) => {
+      state.cityPhoto = action.payload;
+    },
   },
 });
 
-const { setCityName, setWeatherData } = weatherDataStore.actions;
+const { setCityName, setWeatherData, setCityPhoto } = weatherDataStore.actions;
 //异步请求部分
 
 // 调用 API 获取城市名字
@@ -55,6 +59,30 @@ const fetchWeatherData = () => {
   };
 };
 
+const fetchCityPhoto = () => {
+    return async (dispatch, getState) => {
+        const cityPhotoApiKey = "m11T5e9rUfdnl2S3CWAjMNRmyDLM3lH8m4p-ARMBALs";
+        const state = getState();
+        const cityName = state.weatherdata.cityName;
+        console.log(cityName);
+        if (cityName) {
+            try {
+                const apiUrl = `https://api.unsplash.com/search/photos?page=1&query=${cityName}&client_id=${cityPhotoApiKey}`;
+                const cityPhotoRes = await axios.get(apiUrl);
+                dispatch(
+                setCityPhoto(cityPhotoRes.data.results[0].urls.regular)
+                );
+                console.log(cityPhotoRes);
+
+            } catch (error) {
+                console.error("Error fetching photo data:", error);
+
+            }
+        }
+
+    };
+};
+
 const weatherDataReducer = weatherDataStore.reducer;
-export { setCityName,fetchUserCity,fetchWeatherData };
+export { setCityName,fetchUserCity,fetchWeatherData,fetchCityPhoto};
 export default weatherDataReducer;
